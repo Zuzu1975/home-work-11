c3ba2715e81c24e3ec1b516a95b9d1d89dd2d29c
Zuzu work <muratov01@mail.ru>
2023-05-17 22:43:09 +0600

    Домашка 11

---
+++ main.js
@@ -0,0 +1,63 @@
+
+
+                                        //Домашка 10/1.
+
+// let b = ['e','r','i','k','t','h','e','s','t']  
+// let a = new Set(b);
+// for(let key in b){
+//     console.log(b[key]);    
+// }
+
+                                        // Домашка 10/2
+
+
+// function check() {
+// let input = document.querySelector('input');
+// console.log(input.value);
+
+
+//     var x = document.getElementById("example").value;
+//     alert (x);
+// }
+
+                                            // Домашка 10/3  не смогла до конца понять как писать
+
+
+
+// function check() {
+
+//    let input = document.querySelector('input');
+//    let a = new Set();
+//    let b = [7,17,27,77,777]
+   
+//    a.add('привет')
+//    a.add(1)
+//    a.add(11)
+//    a.add(21)  
+//    console.log(input.value );
+//    console.log(a.has());
+  
+// }
+//                                             // Домашка 10/4
+
+
+function check() {
+let input = document.querySelector('input');
+console.log(input.value);
+
+
+let a = new Set();
+let b = [7,77,777,7777,77]
+a.add('привет')
+a.add('как дела')  
+a.add(1)
+a.add(11)
+a.add('привет')          
+console.log(a);
+
+for(let key in b){
+    console.log(b[key]);    
+}
+
+}
+
